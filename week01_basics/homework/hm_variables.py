d954675af3f07c2001b083547ba630c65f75406e
longht2907 <longhuynh2907@gmail.com>
2025-10-30 11:17:03 +0700

    Variables practice

---
+++ week01_basics/homework/hm_variables.py
@@ -0,0 +1,47 @@
+# name = "Thanh Long"
+# age = 29
+# city = "Hồ Chí Minh"
+# print(f"Tôi tên là {name}, năm nay tôi {age} tuổi, và tôi đang sống ở {city}")
+
+"""Tính tổng và trung bình"""
+# a = 10
+# b = 15
+# c = 20
+
+# sum = a + b + c
+# average = sum/3
+# print("Tổng của a,b,c là:", sum)
+# print(f"Tổng của a,b,c là: {sum}")
+# print("Gía trị trung bình của a,b,c là :", average)
+# print(f"Gía trị trung bình của a,b,c là {average}")
+
+"""Kiểm tra kiểu dữ liệu"""
+# x = 10
+# y = 3.14
+# name = "Python"
+# is_active = True
+
+# print(type(x))
+# print(type(y))
+# print(type(name))
+# print(type(is_active))
+
+"""Gán nhiều biến cùng lúc"""
+# q,w,e = 1,2,3
+# print(f"q = {q}, w = {w}, e = {e}")
+
+# count = 0
+
+# def increase():
+#     global count
+#     count += 1
+#     print(f"Gía trị của count là : {count}")
+# increase()
+# increase()
+# increase()
+
+
+
+
+
+
